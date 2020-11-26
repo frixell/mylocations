d6f61ca4819c82e87b116956270bbf067fbc43ac
mosh kainer <mosh.kainer@comm-it.com>
2020-11-26 15:32:54 +0200

    phase 1 - clean router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import ReactLoading from "react-loading";
 import CategoriesPage from '../pages/CategoriesPage';
-// import NotFoundPage from '../containers/NotFoundPage';
 
 const createHistory = require("history").createBrowserHistory;
-
-
-let loadImage = "";
-if (navigator.userAgent.toLowerCase().indexOf('msie') > -1 || navigator.userAgent.toLowerCase().indexOf('trident') > -1 || navigator.userAgent.toLowerCase().indexOf('edge') > -1 ){
-    loadImage = <div style={{width:'100vw', height:'100vh', display:'flex', justifyContent:'center', alignItems:'center'}}><img src="/images/ie-preloader.gif" alt="mylocations" /></div>;
-} else {
-    loadImage = <div style={{width:'100vw', height:'100vh', display:'flex', justifyContent:'center', alignItems:'center'}}><ReactLoading type="spinningBubbles" color="#666665" /></div>;
-}
 
 export const history = typeof(window) !== "undefined" ? createHistory() : createMemoryHistory();
 
@@ -24,7 +14,6 @@ class AppRouter extends React.Component {
                 <div>
                     <Switch>
                         <Route path="/" render={(props) => ( <CategoriesPage {...props} />)} exact={true} />
-                        
                     </Switch>
                 </div>
             </Router>
@@ -33,8 +22,3 @@ class AppRouter extends React.Component {
 };
 
 export default AppRouter;
-
-
-
-{/* <Route path="/:category" render={(props) => ( <CategoryPage {...props} />)} exact={true} />
-                        <Route component={NotFoundPage} /> */}
