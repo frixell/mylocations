4b35fd0e8df744c59dfbd8e48b36184be6b3d8a5
mosh kainer <mosh.kainer@comm-it.com>
2020-11-26 15:46:23 +0200

    phase 1 - not found page

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFoundPage = (props) => {
+
+    return (
+        <div className='categories__container'>
+            404 - page not found! <a href="/">go to myLocations!</a>
+        </div>
+    );
+};
+
+export default NotFoundPage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,23 +2,21 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import CategoriesPage from '../pages/CategoriesPage';
+import NotFoundPage from '../pages/NotFoundPage';
 
 const createHistory = require("history").createBrowserHistory;
 
 export const history = typeof(window) !== "undefined" ? createHistory() : createMemoryHistory();
 
-class AppRouter extends React.Component {
-    render() {
-        return (
-            <Router history={history}>
-                <div>
-                    <Switch>
-                        <Route path="/" render={(props) => ( <CategoriesPage {...props} />)} exact={true} />
-                    </Switch>
-                </div>
-            </Router>
-        )
-    }
+const AppRouter = () => {
+    return (
+        <Router history={history}>
+            <Switch>
+                <Route path="/" render={(props) => ( <CategoriesPage {...props} />)} exact={true} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </Router>
+    )
 };
 
 export default AppRouter;
